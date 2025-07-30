e22061392ba1c5808ee6da721445acde79eeb616
Sabeeh Musharraf <sabeeh.musharraf@hotmail.com>
2025-07-30 16:34:59 +0200

    added the devcontainer folder

---
+++ .devcontainer/container_3/build_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Name of the Docker image to build
+IMAGE_NAME=${IMAGE_NAME:-zettascaletech/roscon2024_workshop}
+
+# Get absolute path to base directory (roscon2024_workshop/docker/ dir)
+BASE_DIR=$(cd "$(dirname "$0")" && pwd)
+
+if [ ! "$(docker images -q "${IMAGE_NAME}")" ]; then
+    echo "${IMAGE_NAME} does not exist. Creating..."
+    docker build -t "$IMAGE_NAME" -f "$BASE_DIR/Dockerfile" "$BASE_DIR"
+else
+    echo "Ignoring, $IMAGE_NAME already exists."
+fi
